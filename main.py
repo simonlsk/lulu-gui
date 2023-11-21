09e965981a4aa0374ec823a5332e94c2ec764bd1
Simon Lousky <simon@dagshub.com>
2023-11-22 00:40:43 +0200

    Browse video and select save sections

--- main.py
+++ main.py
@@ -11,29 +11,35 @@ class VideoSectionApp:
         self.setup_ui()
         self.video_file = ""
         self.sections = []
+        self.current_start = None
+        self.current_end = None
 
     def setup_ui(self):
         # Set window title and size
         self.root.title("Video Section Selector")
-        self.root.geometry("480x320")
+        self.root.geometry("480x480")
 
         # Input Frame
         input_frame = tk.LabelFrame(self.root, text="Input", padx=10, pady=10)
         input_frame.pack(padx=10, pady=5, fill="x")
 
-        # Browse Button
-        self.browse_button = tk.Button(input_frame, text="Browse", command=self.browse_file)
-        self.browse_button.pack(side="right")
+        # File Path Entry
+        self.file_path_entry = tk.Entry(input_frame)
+        self.file_path_entry.pack(side='left', expand=True, padx=(0, 5), fill='x')
+
+        # Open File Button
+        self.open_file_button = tk.Button(input_frame, text="Open Video File", command=self.open_file)
+        self.open_file_button.pack(side='right')
 
         # Start and End Time Entries
-        self.start_time_entry = tk.Entry(input_frame, width=10)
-        self.start_time_entry.pack(side="left", padx=(0, 10))
-        self.end_time_entry = tk.Entry(input_frame, width=10)
-        self.end_time_entry.pack(side="left")
+        # self.start_time_entry = tk.Entry(input_frame, width=10)
+        # self.start_time_entry.pack(side="left", padx=(0, 10))
+        # self.end_time_entry = tk.Entry(input_frame, width=10)
+        # self.end_time_entry.pack(side="left")
 
         # Add Section Button
-        add_section_button = tk.Button(input_frame, text="Add section", command=self.add_section)
-        add_section_button.pack(side="left", padx=10)
+        # add_section_button = tk.Button(input_frame, text="Add section", command=self.add_section)
+        # add_section_button.pack(side="left", padx=10)
 
         # Custom Slider Frame
         self.slider_frame = tk.Frame(self.root)
@@ -58,6 +64,9 @@ class VideoSectionApp:
         self.sections_listbox = tk.Listbox(section_list_frame)
         self.sections_listbox.pack(padx=5, pady=5, fill="both", expand=True)
 
+        # Bind delete key
+        self.sections_listbox.bind_all("<BackSpace>", self.delete_element)
+
         # Process Button
         process_button = tk.Button(self.root, text="Process", command=self.process_sections)
         process_button.pack(pady=5)
@@ -66,12 +75,23 @@ class VideoSectionApp:
         # Record the start value from the slider
         start_timecode = self.slider.get_smpte()
         # You can update the UI or internal state with this value
+        previous_start = self.current_start
+        self.current_start = start_timecode
+        if previous_start is None:
+            self.sections.append(f"{self.current_start}-<waiting for end selection>")
+        else:
+            self.sections[-1] = f"{self.current_start}-<waiting for end selection>"
+            self.sections_listbox.delete(tk.END)
+        self.sections_listbox.insert(tk.END, self.sections[-1])
         print("Start timecode set to:", start_timecode)
 
     def set_end(self):
         # Record the end value from the slider
         end_timecode = self.slider.get_smpte()
         # You can update the UI or internal state with this value
+        self.current_end = end_timecode
+        if self.current_end > self.current_start:
+            self.add_section()
         print("End timecode set to:", end_timecode)
 
     def browse_file(self):
@@ -82,14 +102,15 @@ class VideoSectionApp:
             messagebox.showinfo("File Selected", f"Video file selected: {self.video_file}")
 
     def add_section(self):
-        start = self.start_time_entry.get()
-        end = self.end_time_entry.get()
+        start = self.current_start
+        end = self.current_end
         if start and end:
             section = f"{start}-{end}"
-            self.sections.append(section)
+            self.sections[-1] = section
+            self.sections_listbox.delete(tk.END)
             self.sections_listbox.insert(tk.END, section)
-            self.start_time_entry.delete(0, tk.END)
-            self.end_time_entry.delete(0, tk.END)
+            self.current_start = None
+            self.current_end = None
 
     def process_sections(self):
         if not self.sections:
@@ -100,6 +121,25 @@ class VideoSectionApp:
             return
         # Process sections (placeholder for actual processing)
         messagebox.showinfo("Processing", "The sections will be processed.")
+
+    def open_file(self):
+        # Open a dialog to choose an .mp4 file
+        file_path = filedialog.askopenfilename(
+            title="Select a video file",
+            filetypes=[("MP4 files", "*.mp4")],
+            defaultextension=".mp4"
+        )
+        # If a file is selected, update the entry with the file path
+        if file_path:
+            self.file_path_entry.delete(0, tk.END)
+            self.file_path_entry.insert(0, file_path)
+            self.load_video(file_path)  # Load the video using the previously defined method
+
+    def delete_element(self, arg):
+        selected_indices = self.sections_listbox.curselection()
+        # remove selected item from listbox and sections
+        self.sections_listbox.delete(selected_indices[0], selected_indices[-1])
+        self.sections = self.sections[:selected_indices[0]] + self.sections[selected_indices[-1] + 1:]
 
 
 root = tk.Tk()
